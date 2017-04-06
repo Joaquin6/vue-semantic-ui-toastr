930074ee0b38881d95d3151cab7ae3e6ab838d87
gongzza <gz.us.to@gmail.com>
2017-04-06 17:25:49 +0900

    Refactor plugin

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import ToastrHelper from './ToastrHelper.js'
 
 export default {
   install(Vue, {duration = 3000, container = '.toastr-container', autoshow = true, html = false, position = 'right top'} = {}) {
+    const toastr = new ToastrHelper(duration, container, autoshow, html, position)
     Object.defineProperties(Vue.prototype, {
       $toastr: {
         get() {
-          return new ToastrHelper(duration, container, autoshow, html, position)
+          return toastr
         }
       }
     })
